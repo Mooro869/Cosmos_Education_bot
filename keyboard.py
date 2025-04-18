864565c1a3f8a99e47e96008bbcb8181ca4aafac
MOORO <fedorovmatvey48@gmail.com>
2025-04-18 23:41:22 +0300

    Сделаны кнопки планет, так же найдены изображения каждой из них

--- keyboard.py
+++ keyboard.py
@@ -1,1 +1,32 @@
 from aiogram import types
+
+
+# Кнопки при запуске бота
+start_btn = types.ReplyKeyboardMarkup(resize_keyboard=True)
+planet = types.KeyboardButton('Планеты🌘')
+astronauts = types.KeyboardButton('Космонавты👨‍🚀')
+satellites = types.KeyboardButton('Спутники🛰️')
+start_btn.add(planet, astronauts, satellites)
+
+# Кнопки планет
+planet_btn = types.InlineKeyboardMarkup(row_width=3)
+earth = types.KeyboardButton('Земля', callback_data='earth')
+jupiter = types.KeyboardButton('Юпитер', callback_data='jupiter')
+mars = types.KeyboardButton('Марс', callback_data='mars')
+merkury = types.KeyboardButton('Меркурий', callback_data='merkury')
+neptune = types.KeyboardButton('Нептун', callback_data='neptune')
+plutonium = types.KeyboardButton('Плутон', callback_data='plutonium')
+saturn = types.KeyboardButton('Сатурн', callback_data='saturn')
+uran = types.KeyboardButton('Уран', callback_data='uran')
+venus = types.KeyboardButton('Венера', callback_data='venus')
+planet_btn.add(earth, jupiter, mars, merkury, neptune, plutonium, saturn, uran, venus)
+
+# Кнопки астронавтов
+astronauts_btn = types.InlineKeyboardMarkup(row_width=3)
+
+astronauts_btn.add()
+
+# Кнопки спутников
+satellites_btn = types.InlineKeyboardMarkup(row_width=3)
+
+satellites_btn.add()
--- main.py
+++ main.py
@@ -11,6 +11,70 @@ bot = Bot(token=config.TOKEN_API)
 dp = Dispatcher(bot=bot)
 
 
+@dp.message_handler(commands=['start'])
+async def start_command(message: types.Message):
+    await message.answer(text=f"Здравствуй {message.from_user.full_name}!" + config.START_TEXT,
+                         reply_markup=kb.start_btn)
+
+
+@dp.message_handler(text=['Планеты🌘'])
+async def planet_command(message: types.Message):
+    await message.answer(text='Выберите интересующую вас планету:', reply_markup=kb.planet_btn)
+
+
+@dp.message_handler(text=['Космонавты👨‍🚀'])
+async def astronauts_command(message: types.Message):
+    await message.answer(text='', reply_markup=kb.astronauts_btn)
+
+
+@dp.message_handler(text=['Спутники🛰️'])
+async def satellites_command(message: types.Message):
+    await message.answer(text='', reply_markup=kb.satellites_btn)
+
+
+@dp.callback_query_handler(lambda x: x.data == "earth")
+async def earth(callback_query: types.CallbackQuery):
+    ...
+
+
+@dp.callback_query_handler(lambda x: x.data == "jupiter")
+async def jupiter(callback_query: types.CallbackQuery):
+    ...
+
+
+@dp.callback_query_handler(lambda x: x.data == "mars")
+async def mars(callback_query: types.CallbackQuery):
+    ...
+
+
+@dp.callback_query_handler(lambda x: x.data == "merkury")
+async def merkury(callback_query: types.CallbackQuery):
+    ...
+
+
+@dp.callback_query_handler(lambda x: x.data == "neptune")
+async def neptune(callback_query: types.CallbackQuery):
+    ...
+
+
+@dp.callback_query_handler(lambda x: x.data == "plutonium")
+async def plutonium(callback_query: types.CallbackQuery):
+    ...
+
+
+@dp.callback_query_handler(lambda x: x.data == "saturn")
+async def saturn(callback_query: types.CallbackQuery):
+    ...
+
+
+@dp.callback_query_handler(lambda x: x.data == "uran")
+async def uran(callback_query: types.CallbackQuery):
+    ...
+
+
+@dp.callback_query_handler(lambda x: x.data == "venus")
+async def venus(callback_query: types.CallbackQuery):
+    ...
 
 
 if __name__ == '__main__':
